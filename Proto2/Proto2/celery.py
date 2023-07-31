c4fe17b22dd448239e750b87850b5de0880affed
Auro <aurokumar926@gmail.com>
2023-08-01 05:06:40 +0530

    celery + flower local config solved

--- Proto2/Proto2/celery.py
+++ Proto2/Proto2/celery.py
@@ -2,6 +2,6 @@ import os
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Proto2.settings")
-app = Celery("Proto2")
+app = Celery("Proto2", broker='redis://default:5bcce6315ccc45bdbfa832c9114e42f4@fly-proto2-redis.upstash.io')
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
